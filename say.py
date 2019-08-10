66a9072bced39e9442c5b89ab66ca2040e84bcbf
Tyr Mactire <tyr@pettingzoo.co>
2019-08-09 20:54:40 -0700

    newcaster

--- say.py
+++ say.py
@@ -14,7 +14,7 @@ def say(speech_text):
 
     # Synthesize the sample text, saving it in an MP3 audio file
     polly_client = boto3.client('polly')
-    response = polly_client.synthesize_speech(VoiceId='Brian',
+    response = polly_client.synthesize_speech(VoiceId='Matthew',
                                               Engine='neural',
                                               OutputFormat='mp3',
                                               Text=speech_text)
@@ -34,13 +34,42 @@ def say(speech_text):
 
         os.remove("speech.mp3")
 
+def newscast(speech_text):
+    redis_host = os.getenv('REDIS_HOST', "127.0.0.1")
+    conn = StrictRedis(host=redis_host)
+
+    # Synthesize the sample text, saving it in an MP3 audio file
+    polly_client = boto3.client('polly')
+    response = polly_client.synthesize_speech(VoiceId='Matthew',
+                                              Engine='neural',
+                                              OutputFormat='mp3',
+                                              Text='<speak><amazon:domain name="news">{0}</amazon:domain></speak>'.format(speech_text),
+                                              TextType='ssml')
+
+    with Lock(conn, "talking"):
+        with open('speech.mp3', 'wb') as file:
+            file.write(response['AudioStream'].read())
+
+        print("Got the lock. Doing some work ...")
+
+        pygame.mixer.init()
+        pygame.mixer.music.load("speech.mp3")
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy() == True:
+            continue
+
+        os.remove("speech.mp3")
 
 @app.route('/')
 def hello_world():
     # here we want to get the value of user (i.e. ?user=some-value)
     speech_text = request.args.get('s')
+    news_text = request.args.get('n')
     if speech_text is not None:
         say(speech_text)
+    elif news_text is not None:
+        newscast(news_text)
 
     return 'ok!'
 
