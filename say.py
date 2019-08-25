370aff051d96a2b8bea7e86177f6cc341429a3ea
Tyr Mactire <tyr@pettingzoo.co>
2019-08-25 12:48:48 -0700

    say with beep

--- say.py
+++ say.py
@@ -1,4 +1,5 @@
 import os
+
 import boto3
 import pygame
 from flask import Flask, request
@@ -29,9 +30,48 @@ def say(speaker, speech_text):
         pygame.mixer.music.load("speech.mp3")
         pygame.mixer.music.play()
 
+        # Wait for Auudio to Finish
         while pygame.mixer.music.get_busy() == True:
             continue
 
+        pygame.mixer.quit()
+        os.remove("speech.mp3")
+
+
+def say_w_beep(speaker, speech_text):
+    redis_host = os.getenv('REDIS_HOST', "127.0.0.1")
+    conn = StrictRedis(host=redis_host)
+
+    # Synthesize the sample text, saving it in an MP3 audio file
+    polly_client = boto3.client('polly')
+    response = polly_client.synthesize_speech(VoiceId=speaker,
+                                              Engine='neural',
+                                              OutputFormat='mp3',
+                                              Text=speech_text)
+
+    with Lock(conn, "talking"):
+        with open('speech.mp3', 'wb') as file:
+            file.write(response['AudioStream'].read())
+
+        print("Got the lock. Doing some work ...")
+
+        pygame.mixer.init()
+
+        pygame.mixer.music.load("beep.mp3")
+        pygame.mixer.music.play()
+
+        # Wait for Auudio to Finish
+        while pygame.mixer.music.get_busy() == True:
+            continue
+
+        pygame.mixer.music.load("speech.mp3")
+        pygame.mixer.music.play()
+
+        # Wait for Auudio to Finish
+        while pygame.mixer.music.get_busy() == True:
+            continue
+
+        pygame.mixer.quit()
         os.remove("speech.mp3")
 
 
@@ -69,11 +109,14 @@ def newscast(speaker, speech_text):
 def matthew_say():
     # here we want to get the value of user (i.e. ?user=some-value)
     speech_text = request.args.get('s')
+    speech_w_beep_text = request.args.get('sb')
     news_text = request.args.get('n')
     if speech_text is not None:
         say("Matthew", speech_text)
     elif news_text is not None:
         newscast("Matthew", news_text)
+    elif speech_w_beep_text is not None:
+        say_w_beep("Matthew", speech_w_beep_text)
 
     return 'ok!'
 
@@ -83,11 +126,14 @@ def matthew_say():
 def joanna_say():
     # here we want to get the value of user (i.e. ?user=some-value)
     speech_text = request.args.get('s')
+    speech_w_beep_text = request.args.get('sb')
     news_text = request.args.get('n')
     if speech_text is not None:
         say("Joanna", speech_text)
     elif news_text is not None:
         newscast("Joanna", news_text)
+    elif speech_w_beep_text is not None:
+        say_w_beep("Joanna", speech_w_beep_text)
 
     return 'ok!'
 
